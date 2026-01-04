28746a9107a7ded413f91837618899ef06d3b8e7
Jacob Heflin <btheflin@gmail.com>
2026-01-03 23:12:45 -0600

    Added yaml, toml parsers, additional command line arguments

--- src/qcschema_validator/__init__.py
+++ src/qcschema_validator/__init__.py
@@ -1,33 +1,132 @@
 import argparse
 import json
-import numpy as np
-from .schema_class import schemas
+import sys
+import tomllib
+from pathlib import Path
 from typing import get_origin
-from pydantic import TypeAdapter,ConfigDict
+
+import numpy as np
+import tomli_w
+import yaml
+from pydantic import ConfigDict, TypeAdapter, ValidationError
+
+from .schema_class import schemas
 
 argparse_config = {
     "prog": "qcschema_validator",
     "description": "QCSchema Validation Tool",
 }
 
-parser = argparse.ArgumentParser(**argparse_config)
-parser.add_argument("input_file", type=argparse.FileType("rb"))
+def parse_json(file_path: str):
+    path = Path(file_path)
+    try:
+        with path.open('rb') as f:
+            return json.load(f)
 
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f"Yo this toml file is not here: {path}") from e
+
+    except PermissionError as e:
+        raise PermissionError(f"You ain't allowed to read this file: {path}") from e
+
+    except json.JSONDecodeError:
+        raise ValueError(f"Failed to parse '{path.name}' as JSON\n") from None
+
+def parse_xml(file_path: str):
+    pass
+def parse_yaml(file_path: str):
+    path = Path(file_path)
+    try:
+        with path.open('rb') as f:
+            return yaml.safe_load(f)
+
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f"Yo this toml file is not here: {path}") from e
+
+    except PermissionError as e:
+        raise PermissionError(f"You ain't allowed to read this file: {path}") from e
+
+    except yaml.YAMLError:
+        raise ValueError(f"Failed to parse '{path.name}' as JSON\n") from None
+
+def parse_toml(file_path: str):
+    path = Path(file_path)
+    try:
+        with path.open('rb') as f:
+            return tomllib.load(f)
+
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f"Yo this toml file is not here: {path}") from e
+
+    except PermissionError as e:
+        raise PermissionError(f"You ain't allowed to read this file: {path}") from e
+
+    except tomllib.TOMLDecodeError as e:
+        raise ValueError(f"{path} is either not toml or not valid toml: {e}") from e
+
+PARSERS = {
+    "json": parse_json,
+    "xml": parse_xml,
+    "yaml": parse_yaml,
+    "toml": parse_toml
+}
+
+def infer_filetype(file_path: str):
+    ext = Path(file_path).suffix.lower().lstrip(".")
+    if ext not in PARSERS.keys():
+        raise argparse.ArgumentTypeError("Yeah no way I'm parsing that file")
+    return ext
+
+parser = argparse.ArgumentParser(**argparse_config)
+parser.add_argument(
+                    "input_file",
+                    metavar="[FILE]",
+                    type=str,
+                    help="File to be validated"
+                )
+parser.add_argument(
+                    "--format",
+                    "-f",
+                    type=str,
+                    choices=PARSERS.keys(),
+                    help="Input file format"
+                )
+parser.add_argument(
+                    "--output-file",
+                    "-o",
+                    type=str,
+                    choices=PARSERS.keys(),
+                    help="Input file format"
+                )
+parser.add_argument(
+                    "--out-format",
+                    type=str,
+                    choices=PARSERS.keys(),
+                    help="Input file format"
+                )
+
+if len(sys.argv) == 1:
+    parser.print_help()
+    sys.exit(1)
 
 args = parser.parse_args()
-data = json.loads(args.input_file.read())
+if args.format is None:
+    ext = infer_filetype(args.input_file)
+    parser_func = PARSERS[ext]
+else:
+    parser_func = PARSERS[args.format]
 
+data = parser_func(args.input_file)
 
-def matches(value, annotation):
+def matches(value, annotation) -> bool:
     ta = TypeAdapter(annotation, config=ConfigDict(arbitrary_types_allowed=True))
     try:
         if get_origin(annotation) == np.ndarray or annotation is np.ndarray:
             ta.validate_python(np.asarray(value))
-            return True
         else:
             ta.validate_python(value)
-            return True
-    except:
+        return True
+    except ValidationError:
         return False
 
 def main() -> None:
@@ -35,27 +134,36 @@ def main() -> None:
         if 'schema_name' in obj.model_fields.keys():
             if obj.model_fields['schema_name'].default == data['schema_name']:
                 schema_being_validated = data['schema_name']
+                required_cov = {}
+                optional_cov = {}
                 required_vals = {}
                 optional_vals = {}
                 for name, field in obj.model_fields.items():
                     if name not in data.keys():
                         if field.is_required():
-                            required_vals[name]: False
+                            print(name)
+                            required_cov[name] =  False
+                            required_vals[name] =  None
                             continue
                         else:
-                            optional_vals[name] = False
+                            optional_cov[name] = False
+                            optional_vals[name] = None
                             continue
                     if field.is_required():
-                        required_vals[name] = matches(data[name], field.annotation)
+                        required_cov[name] = matches(data[name], field.annotation)
+                        required_vals[name] = data[name]
                     else:
-                        optional_vals[name] = matches(data[name], field.annotation)
+                        optional_cov[name] = matches(data[name], field.annotation)
+                        optional_vals[name] = data[name]
                         
     print(f"Schema under scrutiny: {schema_being_validated}")
     print("Required Value Coverage:")
-    for key, value in required_vals.items():
-            print("\t", key + ": ", value)
-    print(f"Coverage of Required Values: {sum(required_vals.values()) / len(required_vals.values()):.0%}")
+    for key, value in required_cov.items():
+            print(f"\t{key}: {value}")
+            print(f"\t\tData: {required_vals[key]}", )
+    print(f"Coverage of Required Values: {sum(required_cov.values()) / len(required_cov.values()):.0%}")
     print("Optional Value Coverage:")
-    for key, value in optional_vals.items():
-            print("\t", key + ": ", value)
-    print(f"Coverage of Optional Values: {sum(optional_vals.values()) / len(optional_vals.values()):.0%}")
+    for key, value in optional_cov.items():
+            print(f"\t{key}: {value}")
+            print(f"\t\tData: {optional_vals[key]}", )
+    print(f"Coverage of Optional Values: {sum(optional_cov.values()) / len(optional_cov.values()):.0%}")
---
+++ tests/test_qcschema_validator.py
@@ -0,0 +1,3 @@
+from qcschema_validator import main
+
+
